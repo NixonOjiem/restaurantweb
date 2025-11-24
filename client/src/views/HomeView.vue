cb7c73392d14c9541f9f34569a03b4ab3b5d2cbb
Arnold Kimani <53002068+arnoldqym@users.noreply.github.com>
2025-11-24 14:23:04 +0300

    added code to put footer at the bottom even if there is no data

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,6 +1,11 @@
 <template>
-  <section class="">
+  <section class="flex flex-col min-h-screen">
     <RestaurantNavbar />
+
+    <main class="grow">
+      <h1 class="text-4xl p-10 text-center">Welcome to Cuisine Élégante!</h1>
+      <p class="text-center p-5">This area is empty to push the footer down.</p>
+    </main>
 
     <FooterComponent />
   </section>
