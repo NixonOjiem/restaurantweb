8050f1cc9f26657d36e06a6b9d4051ddaf0db2bf
Nick <nicksonogm@gmail.com>
2025-11-24 12:02:36 +0300

    created basic server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,17 @@
-console.log("Hello, Node.js Project!");
+// 1. Import the Express module
+const express = require("express");
+// Import the router you exported from test.route.js
+const testUrl = require("./src/routes/test.route");
+const app = express();
+
+const PORT = 3000;
+
+// 4. Mount the testUrl router at the '/restaurant' path.
+// All routes defined in test.route.js will be prefixed with '/restaurant'
+app.use("/restaurant", testUrl);
+
+// 5. Start the server and listen on the defined port
+app.listen(PORT, () => {
+  console.log(`✅ Server is running on http://localhost:${PORT}`);
+  console.log("Press Ctrl+C to stop the server.");
+});
