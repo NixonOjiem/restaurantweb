32c3960ac42bc59dba19c86e2dc16d326cc31754
Nick <nicksonogm@gmail.com>
2025-12-10 15:30:53 +0300

    changed the orders routes to support the functionalities in the controller

--- server/src/routes/order.routes.js
+++ server/src/routes/order.routes.js
@@ -1,11 +1,27 @@
 const express = require("express");
 const router = express.Router();
-const { initiateCheckout } = require("../controllers/order.controller");
+const {
+  initiateCheckout,
+  getUserOrders,
+  getAllOrders,
+  updateOrderStatus,
+} = require("../controllers/order.controller");
+const { admin } = require("../middleware/admin.middleware");
 const { protect } = require("../middleware/auth.middleware"); // Ensure user is logged in
 
 router.use(protect);
 
-// POST /restaurant/v1/orders/checkout
+// POST /restaurant/v1/orders/checkout used to post orders
 router.post("/checkout", initiateCheckout);
+
+//Get User Orders (only loggedin users)
+router.get("/myorders", protect, getUserOrders);
+
+//Admin Routes
+//get all orders
+router.get("/admin-orders", protect, admin, getAllOrders);
+
+// Update Status (e.g., mark as DELIVERED)
+router.put("/admin-orders/:id/status", protect, admin, updateOrderStatus);
 
 module.exports = router;
