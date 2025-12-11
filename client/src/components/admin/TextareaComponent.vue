36ae2fdd2368f2c3596a1d926d66ff771ec44c20
maith96 <threedaniel385@gmail.com>
2025-12-11 17:15:14 +0300

    Updated Menu item view and added custom UI components

---
+++ client/src/components/admin/TextareaComponent.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="w-full">
+    <label v-if="label" class="block font-semibold text-xs text-slate-700 mb-1">{{ label }}</label>
+
+    <textarea
+      v-bind="$attrs"
+      v-model="model"
+      class="w-full bg-stone-50 text-stone-800 border border-indigo-200 rounded-lg px-4 py-3 text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:bg-white transition-all duration-200 placeholder-stone-400"
+
+      rows="4"
+    ></textarea>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+
+interface Props {
+  label?: string;
+  modelValue: string | null;
+  placeholder?: string;
+  error?: string | null;
+}
+
+const props = defineProps<Props>();
+const emit = defineEmits(["update:modelValue"]);
+
+const model = computed({
+  get: () => props.modelValue,
+  set: (val: string | null) => emit("update:modelValue", val),
+});
+</script>
--- client/src/views/admin/MenuItemsView.vue
+++ client/src/views/admin/MenuItemsView.vue
@@ -1,5 +1,19 @@
 <template>
-    Menu Items
+   <div class="pt-6 pr-4">
+     <section class="flex items-center justify-between">
+       <div>
+         <h1 class="text-2xl font-bold text-foreground">Menu Items</h1>
+         <p class="text-muted-foreground">Manage your restaurant&apos;s menu</p>
+        </div>
+        <AddMenuItemDialog/>
+    </section>
+   </div>
 </template>
 
-<script setup lang="ts" />
+
+
+<script setup lang="ts" >
+import  AddMenuItemDialog from '@/components/admin/AddMenuItemDialog.vue';
+
+
+</script>
