2fa9b718aff0ab0cbedf83ee671d5e9a8e7b57c2
Nick <nicksonogm@gmail.com>
2025-12-15 10:47:27 +0300

    created the first version of admin orders section

--- client/src/components/admin/OrdersComponent.vue
+++ client/src/components/admin/OrdersComponent.vue
@@ -4,6 +4,10 @@
   </div>
 </template>
 <script setup lang="ts">
-
+import { useAuthStore } from '@/stores/auth';
+const authStore = useAuthStore();
+const token = authStore.token;
+const apiUrl = import.meta.env.VITE_API_BASE_URL;
+const fetchOrdersUrl = `${apiUrl}/orders`;
 </script>
 <style lang="css"></style>
