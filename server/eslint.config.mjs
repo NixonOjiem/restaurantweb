0e1c47ef96dca838b013b675bca1ecd2a6d77ca7
Nick <nicksonogm@gmail.com>
2025-12-20 10:04:51 +0300

    renamed the eslint config file

--- server/eslint.config.mjs
+++ server/eslint.config.mjs
@@ -1,13 +1,13 @@
-// import js from "@eslint/js";
-// import globals from "globals";
-const js = require("@eslint/js");
-const globals = require("globals");
+import js from "@eslint/js";
+import globals from "globals";
 
 export default [
   js.configs.recommended,
   {
     languageOptions: {
       ecmaVersion: 2022,
+      // Since this config is for a Node project using CommonJS (require),
+      // we keep the source type as commonjs for the linter to understand your server files.
       sourceType: "commonjs",
       globals: {
         ...globals.node,
