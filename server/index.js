10d8836388b69d6fcabcc941cac721657bb9761a
Nick <nicksonogm@gmail.com>
2025-12-20 11:14:44 +0300

    created a redirect file to handle browser refreshes stoping the  404 pages

--- server/index.js
+++ server/index.js
@@ -14,6 +14,7 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://127.0.0.1:5174",
   "https://restaurantprod.netlify.app",
+  "https://restaurantprod.netlify.app",
 ];
 const corsOptions = {
   // 1. Define allowed origins
